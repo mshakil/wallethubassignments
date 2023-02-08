802e76f50499840eb566180cfeb7f25d2fcc41f3
mshakil <mshakil74@gmaill.com>
2023-02-09 01:03:53 +0500

    Adding testng dependency

--- src/test/java/org/example/MyDemoTestClass.java
+++ src/test/java/org/example/MyDemoTestClass.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class testngtestDemo {
+public class MyDemoTestClass {
     @Test
     public void HelloWorld(){
         System.setProperty("webdriver.chrome.driver","C:\\drivers\\chrome\\chromedriver.exe");
@@ -12,6 +12,8 @@ public class testngtestDemo {
 
         driver.get("https://facebook.com");
 
+        driver.close();
         driver.quit();
+
     }
 }
