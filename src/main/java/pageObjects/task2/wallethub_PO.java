a601a234a883311cbf7b201c828efb003a7f11c0
mubbashir-10P <mubbashir.shakil@10pearls.com>
2023-02-11 02:33:53 +0500

    Facebook post logic

--- src/main/java/pageObjects/task2/wallethub_PO.java
+++ src/main/java/pageObjects/task2/wallethub_PO.java
@@ -1,5 +1,26 @@
 package pageObjects.task2;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import pageObjects.Base_PO;
 
+import java.util.List;
+
 public class wallethub_PO extends Base_PO {
+    private @FindBy(css=("div[data-pagelet^='FeedUnit_']")) List<WebElement> posts;
+
+    public void FindMyPost(){
+        String postId = null;
+        for (WebElement element: posts) {
+
+            String userName = element.findElement( By.tagName("strong")).findElement(By.tagName("span")).getText();
+            if(userName.equals("Alex Json")){
+                postId = element.getAttribute("id");
+                break;
+            }
+            String customSelector = "div[data-pagelet='"+postId+"'] div[id^='jsc_c'] div.x1vvkbs";
+            String text = getDriver().findElement(By.cssSelector(customSelector)).getText();
+
+        }
+    }
 }
