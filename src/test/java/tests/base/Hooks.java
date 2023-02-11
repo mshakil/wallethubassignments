0464ddd405db6cac1d62d5ee7345edd152ac16c5
mubbashir-10P <mubbashir.shakil@10pearls.com>
2023-02-11 15:38:27 +0500

    changing hooks

--- src/test/java/tests/base/Hooks.java
+++ src/test/java/tests/base/Hooks.java
@@ -1,19 +1,21 @@
 package tests.base;
 
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 
 import static driver.DriverFactory.cleanUpDriver;
 import static driver.DriverFactory.getDriver;
 
 public class Hooks {
-    @BeforeTest
+    @BeforeClass
     public void Setup(){
         getDriver();
         System.out.println("Before Test Worked!");
 
     }
-    @AfterTest
+    @AfterClass
     public void Teardown(){
         cleanUpDriver();
         System.out.println("After Test Worked!");
